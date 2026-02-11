d72e004dddd4acf99692366beb9a9ffc35eb6db6
wasabidejavu <john92160@gmail.com>
2026-02-11 21:45:08 +0100

    Fix build errors: remove BlankFragment, update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,10 @@ buildscript {
     }
 
     dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
         classpath("com.android.tools.build:gradle:8.7.3")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
